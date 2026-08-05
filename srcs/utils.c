1b6e5943b548314766e9fdbfc08d6551bced4481
TioElvis <elvis.veramijares@gmail.com>
2026-08-05 18:20:58 +0200

    fix display error, formatto corretto e' "Error:\n" e non "Error: "

--- srcs/utils.c
+++ srcs/utils.c
@@ -2,7 +2,7 @@
 
 void putstr_error_and_exit(char *error, int exit_code)
 {
-    ft_putstr_fd("Error: ", 1);
+    ft_putstr_fd("Error:\n", 1);
     ft_putstr_fd(error, 1);
     ft_putchar_fd('\n', 1);
 
